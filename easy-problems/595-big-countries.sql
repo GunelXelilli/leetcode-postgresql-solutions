4ba2f78be525d4316ebf074a5a42a10e1872bc60
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-10-31 23:18:10 +0400

    Update 595-big-countries.sql

--- easy-problems/595-big-countries.sql
+++ easy-problems/595-big-countries.sql
@@ -1,1 +1,16 @@
+-- Problem: Big Countries
+-- Link: https://leetcode.com/problems/big-countries/
+-- Difficulty: Easy
+-- Description:
+--   Find the name, population, and area of all big countries.
+--   A country is considered big if it has an area of at least 3,000,000
+--   or a population of at least 25,000,000.
+--   Filter the 'World' table using the WHERE clause.
+
+SELECT 
+    name, 
+    population, 
+    area
+FROM World
+WHERE area >= 3000000 OR population >= 25000000;
 
