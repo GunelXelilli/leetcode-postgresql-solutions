02d04ea15350043e5a613bb20cd154a21085a0dc
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-11-01 00:16:10 +0400

    Update 1757-recyclable-and-low-fat-products.sql

--- easy-problems/1757-recyclable-and-low-fat-products.sql
+++ easy-problems/1757-recyclable-and-low-fat-products.sql
@@ -1,1 +1,13 @@
+-- Problem: Recyclable and Low-Fat Products
+-- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
+-- Difficulty: Easy
+-- Description:
+--   Find all products that are both recyclable and low-fat.
+--   Return their product_id in ascending order.
+
+SELECT product_id
+FROM Products
+WHERE recyclable = 'Y'
+  AND low_fat = 'Y'
+ORDER BY product_id;
 
