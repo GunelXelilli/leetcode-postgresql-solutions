8e223e04d666fe5d975115da143bfb22019e4e6b
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-11-01 00:16:40 +0400

    Update 584-find-customer-referee.sql

--- easy-problems/584-find-customer-referee.sql
+++ easy-problems/584-find-customer-referee.sql
@@ -1,1 +1,13 @@
+-- Problem: Find Customer Referee
+-- Link: https://leetcode.com/problems/find-customer-referee/
+-- Difficulty: Easy
+-- Description:
+--   Find all customers who have a referee (someone who referred them).
+--   Return the customer_id and the referee_id.
+
+SELECT customer_id,
+       referee_id
+FROM Customers
+WHERE referee_id IS NOT NULL
+ORDER BY customer_id;
 
