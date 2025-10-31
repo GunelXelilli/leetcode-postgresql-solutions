8a130ef57c8e3c4161c131162af1817fd3382272
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-10-31 23:20:10 +0400

    Update 1581-customer-who-visited-but-did-not-make-any-transactions.sql

--- easy-problems/1581-customer-who-visited-but-did-not-make-any-transactions.sql
+++ easy-problems/1581-customer-who-visited-but-did-not-make-any-transactions.sql
@@ -1,1 +1,17 @@
+-- Problem: Customer Who Visited but Did Not Make Any Transactions
+-- Link: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+-- Difficulty: Easy
+-- Description:
+--   Write a SQL query to find the number of times each customer visited the store
+--   but did not make any transactions.
+--   Use a LEFT JOIN to include all visit records, and filter out those
+--   where a transaction exists.
+
+SELECT 
+    customer_id,
+    COUNT(visit_id) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions USING (visit_id)
+WHERE transaction_id IS NULL
+GROUP BY customer_id;
 
