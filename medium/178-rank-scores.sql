8cc8073860a6e5196253ee4e96819cf6432fa089
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-10-31 23:13:04 +0400

    Update 178-rank-scores.sql

--- medium/178-rank-scores.sql
+++ medium/178-rank-scores.sql
@@ -1,1 +1,8 @@
+-- Problem: [Problem Name]
+-- Link: [LeetCode Link]
+-- Difficulty: [Easy/Medium/Hard]
+-- Description:
+--   [1–2 lines explaining what the query does.]
+
+select score, dense_rank() over(order by score desc) rank from Scores
 
