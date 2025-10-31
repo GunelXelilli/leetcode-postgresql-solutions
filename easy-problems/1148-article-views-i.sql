d51ea87cbfc4af9a8a2dffaf37ec10f5748ce710
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-11-01 00:14:46 +0400

    Update 1148-article-views-i.sql

--- easy-problems/1148-article-views-i.sql
+++ easy-problems/1148-article-views-i.sql
@@ -1,1 +1,14 @@
+-- Problem: Article Views I
+-- Link: https://leetcode.com/problems/article-views-i/
+-- Difficulty: Easy
+-- Description:
+--   For each author, find the total number of views on their articles.
+--   Return the author's name and the total views.
+
+SELECT a.author_id,
+       COUNT(v.user_id) AS views_count
+FROM Articles a
+LEFT JOIN Views v ON a.article_id = v.article_id
+GROUP BY a.author_id
+ORDER BY a.author_id;
 
