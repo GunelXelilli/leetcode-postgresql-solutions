d73347f8fbafc2f47c6e26142c08be8d6fe23492
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-10-31 23:52:19 +0400

    Update 570-managers-with-at-least-5-direct-reports.sql

--- medium/570-managers-with-at-least-5-direct-reports.sql
+++ medium/570-managers-with-at-least-5-direct-reports.sql
@@ -1,1 +1,16 @@
+-- Problem: Managers with at Least 5 Direct Reports
+-- Link: https://leetcode.com/problems/managers-with-at-least-5-direct-reports/
+-- Difficulty: Easy
+-- Description:
+--   Find the names of managers who have at least 5 direct reports.
+
+WITH managercount AS (
+    SELECT managerId, COUNT(managerId) AS report_count
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(managerId) >= 5
+)
+SELECT name
+FROM Employee
+WHERE id IN (SELECT managerId FROM managercount);
 
