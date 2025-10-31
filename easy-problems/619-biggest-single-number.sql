9fc5d0c8bfa7557755113be46fd8c62eb2214e05
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-11-01 00:20:43 +0400

    Update 619-biggest-single-number.sql

--- easy-problems/619-biggest-single-number.sql
+++ easy-problems/619-biggest-single-number.sql
@@ -5,13 +5,6 @@
 --   Write a solution to find the largest number that appears only once in the MyNumbers table.
 --   If no such number exists, return null.
 
-SELECT 
-    MAX(num) AS num
-FROM (
-    SELECT 
-        num,
-        COUNT(*) AS count_num
-    FROM MyNumbers
-    GROUP BY num
-    HAVING COUNT(*) = 1
-) AS unique_numbers;
+SELECT MAX(num) AS num 
+    FROM ( SELECT num, COUNT(*) AS count_num FROM MyNumbers GROUP BY num HAVING COUNT(*) = 1 ) 
+    AS unique_numbers;
