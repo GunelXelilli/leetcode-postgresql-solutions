aa106cf742dceeff93ef27686d6200234b011cef
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-11-01 00:19:02 +0400

    Update 620-not-boring-movies.sql

--- easy-problems/620-not-boring-movies.sql
+++ easy-problems/620-not-boring-movies.sql
@@ -1,1 +1,13 @@
+-- Problem: Not Boring Movies
+-- Link: https://leetcode.com/problems/not-boring-movies/
+-- Difficulty: Easy
+-- Description:
+--   Report movies with an odd-numbered ID and a description that is not "boring".
+--   Return the result ordered by rating in descending order.
+
+SELECT *
+FROM Cinema
+WHERE MOD(id, 2) = 1
+  AND description != 'boring'
+ORDER BY rating DESC;
 
