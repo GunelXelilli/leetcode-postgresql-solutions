85424e391099e19223e98ba88f2b76cd8ed17924
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-10-31 23:10:33 +0400

    Update 175-combine-two-tables.sql

--- easy-problems/175-combine-two-tables.sql
+++ easy-problems/175-combine-two-tables.sql
@@ -1,1 +1,11 @@
+-- Problem: Combine Two Tables
+-- Link: https://leetcode.com/problems/combine-two-tables/
+-- Difficulty: Easy
+-- Description:
+--   Combine person and address information using LEFT JOIN.
+
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
 
