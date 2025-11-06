e7181a09d0cbab3636b4126634a94bce6c1a34d9
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-11-06 15:00:07 +0400

    Update 602-friend-requests-ii-who-has-the-most-friends.sql

--- medium/602-friend-requests-ii-who-has-the-most-friends.sql
+++ medium/602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,1 +1,21 @@
+-- Problem: Friend Requests II: Who Has the Most Friends
+-- Link: https://leetcode.com/problems/friend-requests-ii-who-has-the-most-friends/
+-- Difficulty: Medium
+-- Description:
+--   Find the person (or people) who have the most friends and the total number of friends they have.
+--   Each row in RequestAccepted means two users became friends:
+--     requester_id ↔ accepter_id
+--   The output should contain the user id and the number of friends (num).
+
+SELECT 
+  id,
+  COUNT(*) AS num
+FROM (
+  SELECT requester_id AS id FROM RequestAccepted
+  UNION ALL
+  SELECT accepter_id AS id FROM RequestAccepted
+) AS all_friends
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1;
 
