944661bf9ddb1d7672a68bbc87c8560f8116bb92
Günel Xəlilli <147483789+GunelXelilli@users.noreply.github.com>
2025-11-06 14:30:07 +0400

    Update 185-department-top-three-salaries.sql

--- hard/185-department-top-three-salaries.sql
+++ hard/185-department-top-three-salaries.sql
@@ -1,1 +1,23 @@
+-- Problem: Department Top Three Salaries
+-- Link:https://leetcode.com/problems/department-top-three-salaries/
+-- Difficulty: Hard
+-- Description:
+--   Report employees who have one of the top three unique salaries in each department.
+--   Return the result table in any order.
+
+SELECT 
+  Department,
+  Employee,
+  Salary
+FROM (
+  SELECT 
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary,
+    DENSE_RANK() OVER(PARTITION BY d.id ORDER BY e.salary DESC) AS drnk
+  FROM Employee e
+  JOIN Department d 
+    ON e.departmentId = d.id
+) ranked
+WHERE drnk <= 3;
 
